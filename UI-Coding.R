cbd50489ee7a246791bd3b11683429e21ed86812
RFurtkamp <rfurtkamp@dir.ca.gov>
2025-08-12 15:16:50 -0700

    UI Update

---
+++ UI-Coding.R
@@ -0,0 +1,40 @@
+## Dashboard UI Coding
+
+library(bslib)
+library(shiny)
+library(shinydashboard)
+library(gitcreds)
+
+gitcreds::gitcreds_set()
+
+## Create Header
+
+header <- 
+  dashboardHeader( title = ('DIR Workplace Fatality Dashboard'),
+                   disable=FALSE,
+                   titleWidth =  550
+  )
+
+header$children[[2]]$children[[2]] <- header$children[[2]]$children[[1]]
+header$children[[2]]$children[[1]] <- tags$a(href='https://www.dir.ca.gov/dosh/',
+                                             tags$image(src=)
+                                             )
+
+## Create Sidebar
+
+sidebar <-
+  dashboardSidebar('Test')
+
+
+## Create Body
+
+body <-
+  dashboardBody('Test')
+
+## Put everything together
+
+ui <- dashboardPage(header, sidebar, body)
+
+server <- function(input, output, session) {}
+
+shinyApp(ui, server)
